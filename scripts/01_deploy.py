cac45cefe4a5d0bc796cdbac43a6ebecc9b8f289
snail-brain <snail-brain@protonmail.com>
2022-02-01 12:33:01 -0600

    deploy and upgrade proxy

--- scripts/01_deploy.py
+++ scripts/01_deploy.py
@@ -1,12 +1,52 @@
+from weakref import proxy
 from scripts.helpful_scripts import *
-from brownie import network, config, Box, ProxyAdmin
+from brownie import (
+    network,
+    Box,
+    ProxyAdmin,
+    TransparentUpgradeableProxy,
+    Contract,
+    BoxV2,
+)
+
 
 def main():
 
-    # Deploy the Proxy Contract
+    # Deploy the Implementation Contract
     account = getAccount()
     print(f"Deploying to {network.show_active()}")
     box = Box.deploy({"from": account})
-    
+
+    # Deploy Proxy Admin Contract
     proxy_admin = ProxyAdmin.deploy({"from": account})
-    
+
+    # If we were to have an initializer, we would encode the parameter data and pass it to the proxy contract
+    # initializer = box.store, 1
+    box_encoded_initializer_function = encode_function_data()
+
+    # Deploy Proxy Contract
+    proxy = TransparentUpgradeableProxy.deploy(
+        box.address,
+        proxy_admin.address,
+        box_encoded_initializer_function,
+        {"from": account, "gas": 1000000},
+    )
+    print(f"Proxy deployed to {proxy}, you can now upgrade to v2!")
+
+    # Interact with implementation contract through our proxy
+    proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
+    proxy_box.store(1, {"from": account})
+    print(proxy_box.retrieve())
+
+    # Deploy upgrade to implementation contract
+    box2 = BoxV2.deploy({"from": account})
+
+    # Upgrade proxy to new implementation
+    upgrade(account, proxy, box2.address, proxy_admin)
+    proxy_box2 = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
+
+    # Make sure proxy uses new implementation
+    proxy_box2.store(1, {"from": account})
+    tx = proxy_box2.increment({"from": account})
+    tx.wait(1)
+    print(proxy_box2.retrieve())
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,8 +1,8 @@
 from brownie import accounts, network, config
-
+import eth_utils
 
 Local_Blockchain_Environments = ["development", "ganache-local"]
-Forked_Environments = ["mainnet-fork"]
+Forked_Environments = ["mainnet-fork", "moonriver-fork"]
 
 
 def getAccount(index=None, id=None):
@@ -10,7 +10,7 @@ def getAccount(index=None, id=None):
         return accounts[index]
     if id:
         return accounts.load(id)  # Select specific account based on its id or index
-        
+
     if (
         network.show_active()
         in Local_Blockchain_Environments  # If using local chain, use the first address provided from that chain
@@ -20,3 +20,32 @@ def getAccount(index=None, id=None):
     return accounts.add(  # Otherwise, use wallet associated with current network
         config["wallets"]["from_key"]
     )
+
+
+def encode_function_data(initializer=None, *args):
+    if len(args) == 0 or not initializer:
+        return eth_utils.to_bytes(hexstr="0x")
+    return initializer.encode_input(*args)
+
+
+def upgrade(account, proxy, new_imp_address, proxy_admin=None, initializer=None, *args):
+    if proxy_admin:
+        if initializer:
+            encoded_function_data = encode_function_data(initializer, *args)
+            tx = proxy_admin.upgradeAndCall(
+                proxy.address,
+                new_imp_address,
+                encoded_function_data,
+                {"from": account},
+            )
+        else:
+            tx = proxy_admin.upgrade(proxy.address, new_imp_address, {"from": account})
+    else:
+        if initializer:
+            encoded_data = encode_function_data(initializer, *args)
+            tx = proxy.upgradeToAndCall(
+                new_imp_address, encoded_data, {"from": account}
+            )
+        else:
+            tx = proxy.upgradeTo(new_imp_address, {"from": account})
+    return tx
